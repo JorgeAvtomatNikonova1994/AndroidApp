9c3db378731af4963a4d8a88d029e9b751874a28
JorgeAvtomatNikonova1994 <41787273+JorgeAvtomatNikonova1994@users.noreply.github.com>
2018-10-01 00:10:32 -0300

    SOURCE

---
+++ src/main/java/bunker/com/ar/bunker/MainActivity_hola_mundo.kt
@@ -0,0 +1,12 @@
+package bunker.com.ar.bunker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_hola_mundo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_hola_mundo)
+    }
+}
---
+++ src/main/java/bunker/com/ar/bunker/MainActivity_lista_urls.kt
@@ -0,0 +1,12 @@
+package bunker.com.ar.bunker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_lista_urls : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_lista_urls)
+    }
+}
